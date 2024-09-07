ba0d27343cd92c2354f67dc1c1f9dec82d6b01ed
Zac Lu <zaclu61@gmail.com>
2024-09-07 12:36:41 -0700

    Update for 貓抓小說

--- crawler.py
+++ crawler.py
@@ -3,11 +3,15 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import re
 import cn2an
-
+# from selenium.webdriver.chrome.options import Options
+# from selenium import webdriver
 from text_extractor import TextExtractor
+from datetime import datetime
 
 # base_url = "https://www.mei8888.com/%e7%ac%ac1%e7%ab%a0-%e6%ad%b9%e6%af%92%e8%b3%a3%e5%ad%90/"
-valid_url_header = "https://www.mei8888.com/%e"
+# base_url = "https://acg1213.com/book_ZnRtdmZFSzN3RGNhblRuOUcvZldHZz09?su=5D1DC277#stwjxjc"
+# valid_url_header = "https://www.mei8888.com/%e"
+valid_url_header = "https://acg1213.com/book"
 # max_pages_count = 50
 
 class WebCrawler:
@@ -27,33 +31,71 @@ class WebCrawler:
             return None
 
     def parse_links(self, html_content, base_url):
-        soup = BeautifulSoup(html_content, 'html.parser')
         links = set()
-        for link in soup.find_all('a', href=True):
-            full_url = urljoin(base_url, link['href'])
-            links.add(full_url)
+        soup = BeautifulSoup(html_content, 'html.parser')
+        context = soup.find('li', attrs={'class': 'nextp'})
+        print("context !!!")
+        print(context)
+        
+        # print("parsed start !!!")
+        
+        prefix = "<a onclick=\"JumpUrl('"
+        suffix = "')\">"
+
+        for text in context:
+            text = str(text)
+            if text.startswith(prefix):
+                start_index = text.index(prefix)
+                end_index = text.index(suffix)
+                url = text[start_index+len(prefix):end_index]
+                links.add(url)
+                
+        # print("parsed end !!!")   
+        
         return links
 
+        # soup = BeautifulSoup(html_content, 'html.parser')
+        # links = set()
+        # for link in soup.find_all('a', href=True):
+        #     full_url = urljoin(base_url, link['href'])
+        #     links.add(full_url)
+        # return links
+
     def crawl(self, max_pages_count=50):
+        print("Crawling start!!!")
+        
+        crawler_count = 1
+        
         while self.urls_to_visit and len(self.visited_urls) < max_pages_count:
             current_url = self.urls_to_visit.pop(0)
-            if current_url in self.visited_urls:
-                continue
             
             if not current_url.startswith(valid_url_header):
                 continue
 
-            print(f"Crawling: {current_url}")
+            if current_url in self.visited_urls:
+                continue
+
+            print("Count:", crawler_count)
+            crawler_count += 1
+            print(f"Crawling: {current_url}\n")
             html_content = self.fetch_page(current_url)
             if not html_content:
                 continue
 
             links = self.parse_links(html_content, current_url)
+            
+            # print("links", links)
+            
             self.urls_to_visit.extend(links - self.visited_urls)
             self.visited_urls.add(current_url)
+            
+        print("Crawling end!!!")
         return self.visited_urls
+        
 
     def add_to_order_context(self, urls):
+        print("add_to_order_context start!!!")
+
         self.ordered_context = dict()
         for url in urls:
             extractor = TextExtractor(url)
@@ -61,6 +103,9 @@ class WebCrawler:
             title = re.search("第.*章", text).group()
             title_to_num = cn2an.cn2an(title[1:-1], "smart")
             self.ordered_context[title_to_num] = text
+            
+        print("add_to_order_context end!!!")
+
         return self.ordered_context
         
         
@@ -69,6 +114,8 @@ if __name__ == "__main__":
     base_url = input("請輸入網址：")
     global max_pages_count
     max_pages_count = int(input("請輸入最大頁數："))
+    
+    start_time = datetime.now()
     
     crawler = WebCrawler(base_url)
     visited_urls = crawler.crawl(max_pages_count)
@@ -84,3 +131,7 @@ if __name__ == "__main__":
         f.write(order_context[index])
         
     f.close()
+    
+    duration = datetime.now() - start_time
+    
+    print("------ Duration: {} -----\n".format(duration))
